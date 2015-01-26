8edef700515da53412011464f734b31bd529a1b4
Lee Whittaker <whittakerlee81@gmail.com>
2015-01-26 16:26:13 -0700

    Additional Changes to maze generation script.

--- UnityProject/Assets/Scripts/MazeBuilder.cs
+++ UnityProject/Assets/Scripts/MazeBuilder.cs
@@ -9,12 +9,16 @@ public class MazeBuilder : MonoBehaviour {
 
 	private Object o;
 
-	private List<List<Object>> ground;
+	private MazeNode start;
+
+	private List<List<Object>> ground, vertWalls, horiWalls;
 
 	// Use this for initialization
 	void Start () {
 
-		ground = new List<List<Object>> ();
+		start = new MazeNode (Random.Range (0, width-1), Random.Range (0, height-1));
+
+		ground = vertWalls = horiWalls = new List<List<Object>> ();
 
 		// ******************** Full Grid Build *************************
 
@@ -22,6 +26,8 @@ public class MazeBuilder : MonoBehaviour {
 			for (int i = 0; i <= width; i++)
 			{
 				List<Object> column = new List<Object>();
+				List<Object> vert = new List<Object>();
+				List<Object> hori = new List<Object>();
 				for (int j = 0; j < height; j++)
 				{
 
@@ -36,11 +42,13 @@ public class MazeBuilder : MonoBehaviour {
 						o = Instantiate (wall, 
 						                 new Vector3(i * 10, (j * 10) - 5, 0), 
 						                 Quaternion.Euler(0, 0, 90)) as Object;
+						hori.Add (o);
 					}
 			// Vertical Walls
 					o = Instantiate (wall, 
 					                 new Vector3((i * 10) - 5, j * 10, 0), 
 					                 Quaternion.Euler(0, 0, 0)) as Object;
+					vert.Add (o);
 				}
 			// Top Horizontal Walls
 				if (i < width){
@@ -48,7 +56,9 @@ public class MazeBuilder : MonoBehaviour {
 					                 new Vector3(i * 10, (height * 10) - 5, 0), 
 					                 Quaternion.Euler(0, 0, 90)) as Object;
 				}
-				ground.Add(column);
+				ground.Add( column );
+				vertWalls.Add ( vert );
+				horiWalls.Add ( hori );
 			}
 		}
 
@@ -61,5 +71,10 @@ public class MazeNode {
 
 	public int x, y;
 	public List<MazeNode> nodes;
+
+	public MazeNode(int a, int b) {
+		x = a;
+		y = b;
+	}
 
 }
