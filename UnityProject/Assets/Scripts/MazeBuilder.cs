71a43637584d99fca36fcd20e022d29fd0146a1d
Lee Whittaker <whittakerlee81@gmail.com>
2015-01-26 23:21:55 -0700

    Completed Random Maze Generation

--- UnityProject/Assets/Scripts/MazeBuilder.cs
+++ UnityProject/Assets/Scripts/MazeBuilder.cs
@@ -5,29 +5,30 @@ using System.Collections.Generic;
 public class MazeBuilder : MonoBehaviour {
 
 	[SerializeField] private int width = 1, height = 1;
-	[SerializeField] private GameObject floor, wall;
+	[SerializeField] private GameObject floor, wall, parent;
 
-	private Object o;
+	private GameObject o;
 
 	private MazeNode start;
 
-	private List<List<Object>> ground, vertWalls, horiWalls;
-
+	private List<List<GameObject>> ground, vertWalls, horiWalls; 
+	private List<MazeNode> maze;
 	// Use this for initialization
 	void Start () {
 
-		start = new MazeNode (Random.Range (0, width-1), Random.Range (0, height-1));
-
-		ground = vertWalls = horiWalls = new List<List<Object>> ();
+		maze = new List<MazeNode>();
+		ground = new List<List<GameObject>> ();
+		vertWalls = new List<List<GameObject>> ();
+		horiWalls = new List<List<GameObject>> ();
 
 		// ******************** Full Grid Build *************************
 
 		if (width > 0 && height > 0) {
 			for (int i = 0; i <= width; i++)
 			{
-				List<Object> column = new List<Object>();
-				List<Object> vert = new List<Object>();
-				List<Object> hori = new List<Object>();
+				List<GameObject> column = new List<GameObject>();
+				List<GameObject> vert = new List<GameObject>();
+				List<GameObject> hori = new List<GameObject>();
 				for (int j = 0; j < height; j++)
 				{
 
@@ -35,26 +36,27 @@ public class MazeBuilder : MonoBehaviour {
 			// Floors
 						o = Instantiate (floor, 
 					                     new Vector3(i * 10, j * 10, 0), 
-					                     Quaternion.Euler(270, 0, 0)) as Object;
+					                     Quaternion.Euler(270, 0, 0)) as GameObject;
 						column.Add (o);
+						maze.Add (new MazeNode(i,j));
 
 			// Horizontal Walls
 						o = Instantiate (wall, 
 						                 new Vector3(i * 10, (j * 10) - 5, 0), 
-						                 Quaternion.Euler(0, 0, 90)) as Object;
+						                 Quaternion.Euler(0, 0, 90)) as GameObject;
 						hori.Add (o);
 					}
 			// Vertical Walls
 					o = Instantiate (wall, 
 					                 new Vector3((i * 10) - 5, j * 10, 0), 
-					                 Quaternion.Euler(0, 0, 0)) as Object;
+					                 Quaternion.Euler(0, 0, 0)) as GameObject;
 					vert.Add (o);
 				}
 			// Top Horizontal Walls
 				if (i < width){
 					o = Instantiate (wall, 
 					                 new Vector3(i * 10, (height * 10) - 5, 0), 
-					                 Quaternion.Euler(0, 0, 90)) as Object;
+					                 Quaternion.Euler(0, 0, 90)) as GameObject;
 				}
 				ground.Add( column );
 				vertWalls.Add ( vert );
@@ -62,19 +64,118 @@ public class MazeBuilder : MonoBehaviour {
 			}
 		}
 
+		foreach (MazeNode node in maze){
+			node.FindAdjacentNodes(maze);
+		}
+
+		// ***************** Create Maze *******************
+
+
+		start = maze[Random.Range(0, maze.Count - 1)];
+		start.start = true;
+		start.makeMaze ();
+	
 		// **************** Delete Walls to Form Maze *******************
+
+		foreach (MazeNode node in maze){
+			if (node.back != null){
+				if (node.back.x != node.x){
+					Destroy (vertWalls[Mathf.Max (node.x, node.back.x)][node.y]);
+				}
+				if (node.back.y != node.y){
+					Destroy (horiWalls[node.x][Mathf.Max (node.y, node.back.y)]);
+				}
+			}
+		}
+
+		// ***************** Set Remaining Object to Maze Parent Object ***************
+
+		foreach (GameObject item in GameObject.FindGameObjectsWithTag("Maze")){
+			item.transform.parent = parent.transform;
+		}
 
 	}
 }
 
 public class MazeNode {
 
+	enum direction
+	{
+		north,
+		east,
+		south,
+		west,
+	}
+
 	public int x, y;
-	public List<MazeNode> nodes;
+	public string xy {
+		get{ return "(" + x + ", " + y + ")"; }
+	} 
+	public bool active = true;
+	public bool start = false;
+
+	public MazeNode[] AdjacentNodes;
+	public MazeNode back;
+
+	public MazeNode() {
+		AdjacentNodes = new MazeNode[4];
+	}
 
 	public MazeNode(int a, int b) {
 		x = a;
 		y = b;
+		AdjacentNodes = new MazeNode[4];
 	}
 
+	public void makeMaze() {
+		active = false;
+
+		List<MazeNode> availableNodes = new List<MazeNode>();
+		foreach (MazeNode node in AdjacentNodes){
+			if (node != null && node.active){
+				availableNodes.Add (node);
+			}
+		}
+		if (availableNodes.Count > 0)
+		{
+			MazeNode next = availableNodes[Random.Range(0, availableNodes.Count)];
+			next.back = this;
+			next.makeMaze();
+		}
+		else if (back != null)
+		{
+			back.makeMaze ();
+		}
+	}
+
+	public void FindAdjacentNodes(List<MazeNode> maze) {
+		foreach (MazeNode node in maze) {
+			if(isAdjacent(node))
+			{
+				AdjacentNodes[(int)AdjacentNodeDirection(node)] = node;
+			}
+		}
+	}
+
+	private bool isAdjacent (MazeNode node)
+	{
+		return (node.x == x && Mathf.Abs (node.y - y) == 1) || (node.y == y && Mathf.Abs (node.x - x) == 1);
+	}
+
+	private direction AdjacentNodeDirection (MazeNode node)
+	{
+		if ( x == node.x ){
+			if (y < node.y){
+				return direction.north;
+			}else{
+				return direction.south;
+			}
+		} else {
+			if ( x < node.x ){
+				return direction.east;
+			}else{
+				return direction.west;
+			}
+		}
+	}
 }
