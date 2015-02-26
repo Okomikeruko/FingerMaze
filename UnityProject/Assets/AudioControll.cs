d6d181ea66fb8ea34263f61806a9c98fd1d5b439
Lee Whittaker <lee@21zero.com>
2015-02-26 10:15:03 -0700

    Added audio events to GUI button clicks

---
+++ UnityProject/Assets/AudioControll.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof(AudioSource))]
+public class AudioControll : MonoBehaviour {
+
+	public List<AudioBit> AudioBits;
+
+	private AudioSource audioSource;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void PlayClip(string audioName)
+	{
+		AudioBit bit = AudioBits.Find (i => i.name == audioName);
+		if (bit != null)
+		{
+			audioSource.clip = bit.audioClip;
+			audioSource.Play();
+		}
+	}
+}
+
+[System.Serializable]
+public class AudioBit 
+{
+	[SerializeField]
+	public string name;
+
+	[SerializeField]
+	public AudioClip audioClip;
+}
