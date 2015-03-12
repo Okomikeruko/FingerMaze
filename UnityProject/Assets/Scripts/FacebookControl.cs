62f898a673e4d6435713fc0086e836dab746e7fd
Lee Whittaker <whittakerlee81@gmail.com>
2015-03-12 10:06:53 -0600

    Reorganize files and restart Facebook Controller script

---
+++ UnityProject/Assets/Scripts/FacebookControl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacebookControl : MonoBehaviour {
+
+	public void PostToFacebook()
+	{
+		Debug.Log("Help");
+		SPShareUtility.FacebookShare("Come Play FINGER MAZE Today!");
+	}
+}
