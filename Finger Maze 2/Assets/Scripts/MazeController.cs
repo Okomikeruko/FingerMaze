16f10c079dd2001fe7acda40dac354d786851562
Lee Whittaker <whittakerlee81@gmail.com>
2016-03-18 15:13:23 -0600

    Add test scene with sample maze map using an array of buttons with sprites

---
+++ Finger Maze 2/Assets/Scripts/MazeController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MazeController : MonoBehaviour {
+    
+    public Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
