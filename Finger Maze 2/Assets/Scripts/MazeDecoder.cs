733b59c074d4063d4facd61abe6b97d3af1cbf03
Lee Whittaker <whittakerlee81@gmail.com>
2016-03-20 16:19:07 -0600

    Upgrade to Unity 5.3.4f1, Decode Maze routes

---
+++ Finger Maze 2/Assets/Scripts/MazeDecoder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MazeDecoder : MonoBehaviour {
+
+    public string code;
+    public Sprite[] sprites;
+    public GameObject button;
+
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < code.Length; i++)
+        {
+            GameObject btn = Instantiate(button);
+            btn.transform.SetParent(transform);
+            btn.GetComponent<Image>().sprite = sprites[HexToInt(code[i])];
+        }
+	}
+	
+    int HexToInt(char hex)
+    {
+        return hex < 'A' ? 
+            hex - '0' : 
+            10 + hex - 'A';
+    }
+}
---
+++ Finger Maze 2/Assets/Scripts/ScrollToScale.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollToScale : MonoBehaviour {
+
+    public float min, max;
+	
+	// Update is called once per frame
+	void Update () {
+	    float x = transform.localScale.x + Input.GetAxis("Mouse ScrollWheel");
+        x = Mathf.Min(max, x);
+        x = Mathf.Max(min, x);
+        transform.localScale = new Vector3(x, x, x);
+	}
+}
